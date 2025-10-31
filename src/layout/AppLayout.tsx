572e552a71fa4fafb773f417bba0725474d819d1
Sachin594 <pandasachin947@gmail.com>
2025-10-31 19:44:03 +0530

    officeSecondCommit

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,55 +1,60 @@
-import { Col, Layout, Menu, Row, theme } from "antd";
-import Sider from "antd/es/layout/Sider";
-import DingtalkOutlined from "@ant-design/icons";
-import React from "react";
-import { Header } from "antd/es/layout/layout";
+import { Layout } from "antd";
+const { Header, Sider, Content } = Layout;
 
 const AppLayout = () => {
-  const {
-    token: { cyan2, cyan4 },
-  } = theme.useToken();
-
-  const TabsList = [
-    {
-      key: "overview",
-      icon: <DingtalkOutlined />,
-      label: "Overview",
-    },
-    {
-      key: "s",
-      icon: <DingtalkOutlined />,
-      label: "Overview",
-    },
-  ];
-
   return (
-    <div style={{ background: cyan2, padding: "10px" }}>
-      <Layout>
-        <Header style={{ background: cyan4 }}>
-          {/* <div>kullllllllllll</div> */}
-        </Header>
-        <Layout>
-          <Sider
-            style={{ background: cyan4 }}
-            breakpoint="lg"
-            collapsedWidth="0"
-            onBreakpoint={(broken) => {
-              console.log(broken);
-            }}
-            onCollapse={(collapsed, type) => {
-              console.log(collapsed, type);
+    <Layout style={{ minHeight: "100vh" }}>
+      {/* Header - full width */}
+      <Header
+        style={{
+          background: "#fff",
+          padding: "0 16px",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 64,
+          display: "flex",
+          alignItems: "center",
+          zIndex: 1000,
+          borderBottom: "1px solid #f0f0f0",
+        }}
+      >
+        <div className="m-4 h-20 w-20 bg-linear-to-r from-indigo-500 via-purple-500 to-pink-500"></div>
+      </Header>
+
+      {/* Body Layout */}
+      <Layout style={{ marginTop: 64 }}>
+        {/* Sider */}
+        <Sider
+          width={200}
+          style={{
+            background: "#fafafa",
+            borderRight: "1px solid #f0f0f0",
+            height: "calc(100vh - 64px)",
+            position: "fixed",
+            left: 0,
+            top: 64,
+            overflow: "auto",
+          }}
+        >
+          Sider Content
+        </Sider>
+
+        {/* Content */}
+        <Layout style={{ marginLeft: 200 }}>
+          <Content
+            style={{
+              padding: "24px",
+              minHeight: "calc(100vh - 64px)",
+              background: "#fff",
             }}
           >
-            <Menu
-              theme="dark"
-              mode="inline"
-              defaultSelectedKeys={["4"]}
-              items={TabsList}
-            />
-          </Sider>
+            Main Content
+          </Content>
         </Layout>
       </Layout>
-    </div>
+    </Layout>
   );
 };
 
